65ef4a6129360fb4dbb5fd64c57d9b6f93062ea4
that-apex <thatapex.dev@gmail.com>
2021-03-03 17:28:05 +0100

    Add more information to ChessPieceColor

--- chess-lib/src/main/java/net/thatapex/chesssite/chess/pieces/ChessPieceColor.java
+++ chess-lib/src/main/java/net/thatapex/chesssite/chess/pieces/ChessPieceColor.java
@@ -3,17 +3,19 @@ package net.thatapex.chesssite.chess.pieces;
 import net.thatapex.chesssite.chess.board.coordinate.ChessRank;
 
 public enum ChessPieceColor {
-    WHITE(1, ChessRank.fromRankNumber(1).orElseThrow(), ChessRank.fromRankNumber(2).orElseThrow()),
-    BLACK(-1, ChessRank.fromRankNumber(8).orElseThrow(), ChessRank.fromRankNumber(7).orElseThrow());
+    WHITE(1, ChessRank.fromRankNumber(1).orElseThrow(), ChessRank.fromRankNumber(2).orElseThrow(), ChessRank.fromRankNumber(8).orElseThrow()),
+    BLACK(-1, ChessRank.fromRankNumber(8).orElseThrow(), ChessRank.fromRankNumber(7).orElseThrow(), ChessRank.fromRankNumber(1).orElseThrow());
 
     private final int       rankAdvanceDirection;
     private final ChessRank pieceStartingRank;
     private final ChessRank pawnStartingRank;
+    private final ChessRank promotionRank;
 
-    ChessPieceColor(final int rankAdvanceDirection, final ChessRank pieceStartingRank, final ChessRank pawnStartingRank) {
+    ChessPieceColor(final int rankAdvanceDirection, final ChessRank pieceStartingRank, final ChessRank pawnStartingRank, final ChessRank promotionRank) {
         this.rankAdvanceDirection = rankAdvanceDirection;
         this.pieceStartingRank    = pieceStartingRank;
         this.pawnStartingRank     = pawnStartingRank;
+        this.promotionRank        = promotionRank;
     }
 
     public int getRankAdvanceDirection() {
@@ -26,5 +28,25 @@ public enum ChessPieceColor {
 
     public ChessRank getPawnStartingRank() {
         return this.pawnStartingRank;
+    }
+
+    public ChessRank getPromotionRank() {
+        return this.promotionRank;
+    }
+
+    /**
+     * Returns the color opposite to this one
+     *
+     * @return the color opposite to this one
+     */
+    ChessPieceColor getOpposite() {
+        switch (this) {
+            case WHITE:
+                return ChessPieceColor.BLACK;
+            case BLACK:
+                return ChessPieceColor.WHITE;
+            default:
+                throw new IllegalStateException("Illegal color");
+        }
     }
 }
